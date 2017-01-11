9f21ac759337b30d1ce4167cf1c301e1b336206f
AmrAbed <amraabed@gmail.com>
2017-01-11 12:45:27 -0500

    Update Engineering page

--- engineering/index.js
+++ engineering/index.js
@@ -2,10 +2,211 @@
  * Created by AmrAbed on 10/22/16.
  */
 
+/** Load Enginineering Positions
+ *
+ * @param file JSON file to read data from
+ * @param id Target section ID
+ * @param title Target section title
+ */
+function loadEngineeringPositions(file, id, title) {
+    d3.json(file, function (json) {
+        var positions = d3.select(id);
+
+        setHeading(positions, title);
+
+        var media = getContainer(positions, "container")
+            .selectAll("div")
+            .data(json.positions)
+            .enter()
+            .append("div")
+            .attr("class", "row")
+            .append("div")
+            .attr("class","col-lg-12");
+
+        var project = getContainer(media, "col-lg-10");
+
+        project.append("h3")
+            .text(function (d) {
+                return d.position;
+            });
+
+        project.append("a")
+            .attr("target", "_blank")
+            .attr("href", function (d) {
+                return d.project.url;
+            })
+            .append("h4")
+            .text(function (d) {
+                return d.project.name;
+            });
+
+        project.append("p")
+            .attr("class", "text-muted")
+            .text(function (d) {
+                return d.duration;
+            });
+
+        project.append("a")
+            .attr("class", "btn btn-link")
+            .attr("data-toggle", "collapse")
+            .attr("data-target", function (d) {
+                return "#" + d.id;
+            })
+            .text("+ Details");
+
+        getContainer(project, "row")
+            .attr("id", function (d) {
+                return d.id;
+            })
+            .attr("class", "collapse out")
+            .append("p")
+            .append("em")
+            .append("ul")
+            .attr("class", "list list-default")
+            .selectAll("li")
+            .data(function (d) {
+                return d.tasks;
+            })
+            .enter()
+            .append("li")
+            .attr("class", "text-justify")
+            .text(function (d) {
+                return d;
+            });
+
+        project.append("p")
+            .attr("class", "list-inline")
+            .selectAll("li")
+            .data(function (d) {
+                return d.skills;
+            })
+            .enter()
+            .append("li")
+            .append("label")
+            .attr("class", "label label-info")
+            .style("margin-right", "-5px")
+            .text(function (d) {
+                return d;
+            });
+
+        var organization = getContainer(media, "col-lg-2");
+
+        organization.selectAll("a")
+            .data(function (d) {
+                return d.organization;
+            })
+            .enter()
+            .append("a")
+            .attr("href", function (d) {
+                return d.url;
+            })
+            .attr("title", function (d) {
+                return d.name;
+            })
+            .attr("alt", function (d) {
+                return d.name;
+            })
+            .attr("target", "_blank")
+            .append("img")
+            .attr("src", function (d) {
+                return d.logo;
+            })
+            .style("width: 128px;");
+    });
+}
+
+/** Load Products
+ *
+ * @param file JSON file to read data from
+ * @param id Target section ID
+ * @param title Target section title
+ */
+function loadProducts(file, id, title) {
+    d3.json(file, function (json) {
+        var products = d3.select(id);
+
+        setHeading(products, title);
+
+        var product = products.append("div")
+            .attr("class", "container")
+            .selectAll("div")
+            .data(json.products)
+            .enter()
+            .append("div")
+            .attr("class", "row")
+            .append("div")
+            .attr("class", "col-lg-12");
+
+        var header = product.append("h3");
+
+        header.append("a").append("em")
+            .attr("target", "_blank")
+            .attr("href", function (d) {
+                return d.url;
+            })
+            .text(function (d) {
+                return d.name;
+            });
+
+        // product.append("p")
+        //     .attr("class", "text-muted")
+        //     .text(function (d) {
+        //         return d.release;
+        //     });
+        header.append("a")
+            .style("display", function (d) {
+                return d.playstore ? null : "none";
+            })
+            .attr("target", "_blank")
+            .attr("href", function (d) {
+                return d.playstore;
+            })
+            .append("img")
+            .style("width", "150px")
+            .attr("alt", "Get in on Google Play")
+            .attr("src", "../style/img/google-play-badge.png");
+
+        product.append("p")
+            .attr("class", "text-justify")
+            .text(function (d) {
+                return d.description;
+            });
+
+        product.append("div")
+            .append("a")
+            .attr("class", "github-button")
+            .attr("href", function (d) {
+                return d.url;
+            })
+            .attr("data-style", "mega")
+            .attr("aria-label", function (d) {
+                return d.name + " on Github"
+            })
+            .text(function (d) {
+                return d.name;
+            });
+
+        product.append("p")
+            .attr("class", "list-inline")
+            .selectAll("li")
+            .data(function (d) {
+                return d.skills;
+            })
+            .enter()
+            .append("li")
+            .append("label")
+            .attr("class", "label label-info")
+            .style("margin-right", "-5px")
+            .text(function (d) {
+                return d;
+            });
+    });
+}
+
 $.getScript("../include.js", function () {
     populateNavbar();
     loadEngineeringPositions("projects.json", "#positions", "Positions");
-    loadResearchProjects("../research/projects.json", "#projects", "Research Projects");
+    // loadResearchProjects("../research/projects.json", "#projects", "Research Projects");
     loadProducts("projects.json", "#products", "Products");
     loadFooter("../footer.html")
 });
