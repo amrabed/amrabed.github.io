e2652b01a2ff4d16d07f1624e274f9e80c48c89d
Amr Abed <amraabed@gmail.com>
2024-08-11 14:50:48 -0400

    Fix social profiles

--- next/components/Social.jsx
+++ next/components/Social.jsx
@@ -5,17 +5,17 @@ import { profiles } from "@/data/profiles";
 
 const SocialMedia = () => (
   <Fragment>
-    <div className='fixed right-10 bottom-10 flex text-center flex-col gap-4 z-10'>
-      {profiles.map((profile) => (
-        <Link className='w-fit' href={profile.link} key={profile.name} target="_blank" rel="noreferrer">
-          <div
-            className={"p-2 rounded-full text-xl text-white"}
-            style={{ background: "#000000" }}
-          >
-            {profile.icon}
-          </div>
-        </Link>
-      ))}
+    <div className="flex justify-center">
+      <div className='fixed bottom-10 flex flex-row gap-4 z-10'>
+        {profiles.map((profile) => (
+          <Link className='w-fit' href={profile.link} key={profile.name} target="_blank" rel="noreferrer">
+            <div className="p-2 rounded-full text-xl text-white bg-black">
+              {profile.icon}
+            </div>
+          </Link>
+        ))}
+      </div>
+
     </div>
   </Fragment>
 );
