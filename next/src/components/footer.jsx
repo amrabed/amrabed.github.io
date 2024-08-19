485c370e249537b5c666b1dbb433f8e9ab3bb6ae
Amr Abed <amraabed@gmail.com>
2024-08-19 08:18:36 -0400

    Fix footer

--- next/src/components/footer.jsx
+++ next/src/components/footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <div className="p-2 font-sans text-sm left-15 bottom-10">
+  <div className="p-2 text-sm left-15 bottom-0 gap-4">
     &copy; {new Date().getFullYear()} Amr Abed - Built with{" "}
     <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
       Next.js
