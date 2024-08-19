3786a1ffe1c6b0af07c716a47c840319d7cd6336
Amr Abed <amraabed@gmail.com>
2024-08-18 20:55:04 -0400

    Fix experience view

--- next/src/components/timeline.jsx
+++ next/src/components/timeline.jsx
@@ -1,10 +1,10 @@
 const Timeline = ({ items }) => (
-  <div className="container mx-auto">
+  <div className="flex w-100 justify-center">
     <ul className="list-none">
       {items.map((item) => (
         <li className="mb-5" key={item.id}>
           <div className="">
-            <span className="relative left-[-170px] top-20 text-zinc">
+            <span className="relative left-5 xl:left-[-170px] xl:top-5 text-zinc">
               {item.duration}
             </span>
             <div
@@ -12,12 +12,12 @@ const Timeline = ({ items }) => (
               style={{ left: "9px" }}
             ></div>
           </div>
-          <div className="flex group items-center ">
+          <div className="flex group">
             <div className="bg-gray-800 dark:bg-gray-600 group-hover:bg-primary z-10 rounded-full border-4 border-black dark:border-gray-800 h-5 w-5">
               <div className="bg-black dark:bg-gray-800 h-1 w-6 items-center ml-4 mt-1"></div>
             </div>
             <div className="flex-auto ml-6 z-10 font-medium">
-              <div className="order-1 space-y-2 bg-gray-800 group-hover:bg-primary rounded-lg shadow-only transition-ease lg:w-5/12 px-6 py-4">
+              <div className="order-1 space-y-2 bg-gray-800 group-hover:bg-primary rounded-lg shadow-only transition-ease px-6 py-4">
                 <h3 className="mb-3 font-semibold text-white text-lg">
                   {item.position}
                 </h3>
@@ -25,7 +25,7 @@ const Timeline = ({ items }) => (
                   {item.organization.name}
                 </p>
                 <hr />
-                <ul className="list-unstyled">
+                <ul className="">
                   <li className="text-sm font-medium leading-snug tracking-wide text-gray-300 text-opacity-100">
                     {item.tasks[0]}
                   </li>
