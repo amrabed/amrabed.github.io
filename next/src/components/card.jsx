ed94eefee2e4b9e63a0d65898f00bd252a83e014
Amr Abed <amraabed@gmail.com>
2024-08-20 06:59:47 -0400

    Clean up code

--- next/src/components/card.jsx
+++ next/src/components/card.jsx
@@ -2,72 +2,54 @@
 
 import Link from "next/link";
 
-import React, { useState, useEffect } from "react";
 import { FaGithub } from "react-icons/fa6";
 
-import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
 
 import areas from "@/data/areas";
-import roles from "@/data/roles";
 import skills from "@/data/skills";
 
-const Card = ({ item }) => {
-  const [isHidden, setHidden] = useState(false);
-  const onClick = () => {
-    setHidden(!isHidden);
-  };
-
-  return (
-    <div className="flex-fit ml-6 font-medium w-screen md:w-1/2">
-      <div className="dark:bg-gray-800 bg-gray-100 group-hover:bg-primary rounded-lg shadow-only transition-ease px-6 py-4">
-        <h3 className="mb-3 font-semibold text-zinc text-2xl">
-          <div className="flex  justify-between">
-            <div className="flex md:flex-row gap-4 text-xl">
-              <div className="text-primary dark:text-primary-dark">{item.name}</div>
-              <ul className="flex flex-row gap-2 text-2xl">
-                {item.tools.map((tool) => (
-                  <li title={tool} key={tool}>{skills[tool.toLowerCase()]?.icon}</li>
-                ))}
-              </ul>
-            </div>
-            <Link href={`https://github.com/${item.github}`} target="_blank">
-              <FaGithub />
-            </Link>
+const Card = ({ item }) => (
+  <div className="flex-fit ml-6 font-medium w-screen md:w-1/2">
+    <div className="dark:bg-gray-800 bg-gray-100 group-hover:bg-primary rounded-lg shadow-only transition-ease px-6 py-4">
+      <h3 className="mb-3 font-semibold text-zinc text-2xl">
+        <div className="flex  justify-between">
+          <div className="flex md:flex-row gap-4 text-xl">
+            <div className="text-primary dark:text-primary-dark">{item.name}</div>
+            <ul className="flex flex-row gap-2 text-2xl">
+              {item.tools.map((tool) => (
+                <li title={tool} key={tool}>{skills[tool.toLowerCase()]?.icon}</li>
+              ))}
+            </ul>
           </div>
-        </h3>
-        {/* <button onClick={onClick}>
-                <ChevronDownIcon className="size-6" hidden={!isHidden} />
-                <ChevronUpIcon className="size-6" hidden={isHidden} />
-            </button> */}
+          <Link href={`https://github.com/${item.github}`} target="_blank">
+            <FaGithub />
+          </Link>
+        </div>
+      </h3>
 
-        <p className={`pb-4 text-sm text-zinc ${isHidden && "hidden"}`}>
-          {item.description}
-        </p>
-        <ul
-          className={`flex flex-row gap-2 p-1 text-md ${isHidden ? "hidden" : null}`}
-        >
-          {item.tags.map((tag) => (
-            <li key={tag} className="size-6 text-zinc" title={tag}>
-              {areas[tag.toLowerCase()]?.icon}
+      <p className="pb-4 text-sm text-zinc">{item.description}</p>
+      <ul className="flex flex-row gap-2 p-1 text-md">
+        {item.tags.map((tag) => (
+          <li key={tag} className="size-6 text-zinc" title={tag}>
+            {areas[tag.toLowerCase()]?.icon}
+          </li>
+        ))}
+      </ul>
+      <div className="flex justify-between">
+        <ul className="flex flex-row justify-start">
+          {item.roles.map((role) => (
+            <li key={role}>
+              <p className="p-1 text-sm text-zinc-500">{role}</p>
             </li>
           ))}
         </ul>
-        <div className="flex justify-between">
-          <ul className="flex flex-row justify-start">
-            {item.roles.map((role) => (
-              <li key={role}>
-                <p className="p-1 text-sm text-zinc-500">{role}</p>
-              </li>
-            ))}
-          </ul>
 
-          <p className="text-zinc-500 text-end">
-            {new Date(item.released).getFullYear()}
-          </p>
-        </div>
+        <p className="text-zinc-500 text-end">
+          {new Date(item.released).getFullYear()}
+        </p>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
--- next/src/components/timeline.jsx
+++ next/src/components/timeline.jsx
@@ -1,3 +1,43 @@
+"use client";
+
+import React, { useState } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
+
+const Card = ({ item }) => {
+  const [isHidden, setHidden] = useState(true);
+  const onClick = () => {
+    setHidden(!isHidden);
+  };
+
+  return (
+    <div className="flex-auto ml-6 z-10 font-medium">
+      <div className="order-1 space-y-2 bg-gray-800 group-hover:bg-primary rounded-lg shadow-only transition-ease px-6 py-4">
+        <h3 className="mb-3 font-semibold text-white text-lg">
+          {item.position}
+        </h3>
+        <p className="pb-4 text-sm text-gray-100">
+          {item.organization.name}
+        </p>
+        <hr />
+        <ul className="">
+          <li className="text-sm font-medium leading-snug tracking-wide text-gray-300 text-opacity-100">
+            {item.tasks[0]}
+          </li>
+          {/* <button onClick={onClick}>
+            <ChevronDownIcon className="size-6" hidden={!isHidden} />
+            <ChevronUpIcon className="size-6" hidden={isHidden} />
+          </button>
+          {item.tasks.map((task) => (
+            <li className={"text-sm font-medium leading-snug tracking-wide text-gray-300 text-opacity-100" + isHidden && "hidden"}>
+              {task}
+            </li>
+          ))} */}
+        </ul>
+      </div>
+    </div >
+  );
+};
+
 const Timeline = ({ items }) => (
   <div className="flex w-100 justify-center">
     <ul className="list-none">
@@ -16,25 +56,7 @@ const Timeline = ({ items }) => (
             <div className="bg-gray-800 dark:bg-gray-600 group-hover:bg-primary z-10 rounded-full border-4 border-black dark:border-gray-800 h-5 w-5">
               <div className="bg-black dark:bg-gray-800 h-1 w-6 items-center ml-4 mt-1"></div>
             </div>
-            <div className="flex-auto ml-6 z-10 font-medium">
-              <div className="order-1 space-y-2 bg-gray-800 group-hover:bg-primary rounded-lg shadow-only transition-ease px-6 py-4">
-                <h3 className="mb-3 font-semibold text-white text-lg">
-                  {item.position}
-                </h3>
-                <p className="pb-4 text-sm text-gray-100">
-                  {item.organization.name}
-                </p>
-                <hr />
-                <ul className="">
-                  <li className="text-sm font-medium leading-snug tracking-wide text-gray-300 text-opacity-100">
-                    {item.tasks[0]}
-                  </li>
-                  <li className="text-sm font-medium leading-snug tracking-wide text-gray-300 text-opacity-100">
-                    {item.tasks[1]}
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <Card item={item} />
           </div>
         </li>
       ))}
