8c059a0a9091c11ed9398cbd96b7e7f28ffbbe56
Amr Abed <amraabed@gmail.com>
2024-08-20 06:42:11 -0400

    Change skills into map

--- next/src/data/skills.js
+++ next/src/data/skills.js
@@ -11,77 +11,77 @@ import {
   SiGnubash,
 } from "react-icons/si";
 
-const skills = [
-  {
+const skills = {
+  python: {
     name: "Python",
     icon: <FaPython />,
     color: "#3776AB",
   },
-  {
+  tensorflow: {
     name: "TensorFlow",
     icon: <SiTensorflow />,
     color: "#FF6F00",
   },
-  {
+  aws: {
     name: "AWS",
     icon: <FaAws />,
     color: "#FF9900",
   },
-  {
+  "google cloud": {
     name: "Google Cloud",
     icon: <SiGooglecloud />,
     color: "#4285F4",
   },
-  {
+  docker: {
     name: "Docker",
     icon: <FaDocker />,
     color: "#2496ED",
   },
-  {
+  kubernetes: {
     name: "Kubernetes",
     icon: <SiKubernetes />,
     color: "#326CE5",
   },
-  {
+  firebase: {
     name: "Firebase",
     icon: <SiFirebase />,
     color: "#FFC107",
   },
-  {
+  android: {
     name: "Android",
     icon: <FaAndroid />,
     color: "#A4C639",
   },
-  {
+  kotlin: {
     name: "Kotlin",
     icon: <SiKotlin />,
     color: "#7F52A2",
   },
-  {
+  java: {
     name: "Java",
     icon: <FaJava />,
     color: "#B00040",
   },
-  {
+  c: {
     name: "C/C++",
     icon: <SiCplusplus />,
     color: "#00599C",
   },
-  {
+  swift: {
     name: "Swift",
     icon: <DiSwift />,
     color: "#FF9900",
   },
-  {
+  flutter: {
     name: "Flutter",
     icon: <SiFlutter />,
     color: "#02569B",
   },
-  {
+  bash: {
     name: "Bash",
     icon: <SiGnubash />,
     color: "#4EAA25",
   },
-];
+};
 
 export default skills;
--- next/src/sections/skills.jsx
+++ next/src/sections/skills.jsx
@@ -3,7 +3,7 @@ import skills from "@/data/skills";
 
 const Skills = () => (
   <Section id="skills" title="Technical Skills" className="justify-center">
-    {skills.map((skill) => (
+    {Object.values(skills).map((skill) => (
       <div
         className="transition-all duration-700 section-item md:py-5 w-[120px] md:w-[150px]"
         key={skill.name}
