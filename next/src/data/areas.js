d9e7829d64d320b5de9114406dab787a935369bd
Amr Abed <amraabed@gmail.com>
2024-08-21 22:30:24 -0400

    Enrich projects

--- next/src/data/areas.js
+++ next/src/data/areas.js
@@ -1,3 +1,5 @@
+import { color } from "framer-motion";
+
 import { BiBrain } from "react-icons/bi";
 import colors from "tailwindcss/colors";
 
@@ -6,6 +8,8 @@ import {
   DevicePhoneMobileIcon,
   CircleStackIcon,
   ShieldCheckIcon,
+  CodeBracketIcon,
+  Cog6ToothIcon,
 } from "@heroicons/react/24/outline";
 
 const areas = {
@@ -17,9 +21,17 @@ const areas = {
     icon: <BiBrain className="text-2xl" />,
     color: colors.amber,
   },
+  programming: {
+    icon: <CodeBracketIcon />,
+    color: colors.cyan,
+  },
   mobile: {
     icon: <DevicePhoneMobileIcon />,
     color: "green",
+  },
+  devops: {
+    icon: <Cog6ToothIcon />,
+    color: colors.purple,
   },
   database: {
     icon: <CircleStackIcon />,
