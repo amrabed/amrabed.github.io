1ca93fa8629e476f3fbbb9481d1db976cb3784ce
Amr Abed <amraabed@gmail.com>
2024-08-24 07:42:38 -0400

    Search projects by name, role, skill, or tool

---
+++ next/src/types/project.ts
@@ -0,0 +1,15 @@
+type Project = {
+  id: string;
+  name: string;
+  description: string;
+  date: string;
+  roles: string[];
+  tags: string[];
+  tools: string[];
+  github: string;
+  demo: string;
+  homepage: string;
+  group: number;
+};
+
+export default Project;
