f109d2ab67e764415ee357e939f6b7a92b0aeb55
Amr Abed <amraabed@gmail.com>
2024-09-30 19:39:38 -0400

    Fix social bar placement

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -4,25 +4,23 @@ import profiles from "@/data/profiles";
 
 const SocialMedia = () => (
   <>
-    <div className="w-full fixed bottom-0 p-4 z-10">
-      <div className="flex flex-row justify-center">
-        {profiles.map((profile, index) => (
-          <Tooltip key={index} content={profile.name}>
-            <Link href={profile.link} isExternal rel="noreferrer">
-              <div className="bg-background rounded-full p-1">
-                <Button
-                  isIconOnly
-                  variant="light"
-                  radius="full"
-                  className="md:text-xl"
-                >
-                  {profile.icon}
-                </Button>
-              </div>
-            </Link>
-          </Tooltip>
-        ))}
-      </div>
+    <div className="w-full p-2 z-10 flex flex-wrap justify-center items-center">
+      {profiles.map((profile, index) => (
+        <Tooltip key={index} content={profile.name}>
+          <Link href={profile.link} isExternal rel="noreferrer">
+            <div className="bg-background rounded-full p-1">
+              <Button
+                isIconOnly
+                variant="light"
+                radius="full"
+                className="md:text-xl text-foreground"
+              >
+                {profile.icon}
+              </Button>
+            </div>
+          </Link>
+        </Tooltip>
+      ))}
     </div>
   </>
 );
